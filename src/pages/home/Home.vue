a1274a55b4c11ccb62a2ad2c0020e88709e55e3d
songjunchao-cn <songjunchao-cn@gmail.com>
2018-06-22 22:58:28 +0800

    更新header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div>hello world</div>
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
