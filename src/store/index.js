0fbbb0f31891ba21c217304e09ce3031afa5d6de
songjunchao-cn <songjunchao.cn@gmail.com>
2018-06-26 19:30:41 +0800

    city-keepalive finished

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
-
-let defaultCity = '杭州'
-try {
-  if (localStorage.city) {
-    defaultCity = localStorage.city
-  }
-} catch (e) {}
 export default new Vuex.Store({
-  state: {
-    city: defaultCity
-  },
-  actions: {
-    changeCity (ctx, city) {
-      ctx.commit('changeCity', city)
-    }
-  },
-  mutations: {
-    changeCity (state, city) {
-      state.city = city
-      try {
-        localStorage.city = city
-      } catch (e) {}
-    }
-  }
+  state,
+  actions,
+  mutations
 })
